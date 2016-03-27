7db0e6ae4ec79748cb31a87764f47c126b2139df
John Molakvoæ <malabarth@hotmail.fr>
2016-03-27 08:26:28 +0200

    Better colors randomisation. New generator based on the same color tones
    Fix #300
    Only keep numbers and a-f letters used in uids

--- js/filters/contactColor_filter.js
+++ js/filters/contactColor_filter.js
@@ -1,41 +1,42 @@
 angular.module('contactsApp')
 .filter('contactColor', function() {
 	return function(input) {
-		function hslToRgb(h, s, l) {
-			var r, g, b;
-			if (s === 0) {
-				r = g = b = l;
+		function rgbToHsl(r, g, b) {
+			r /= 255, g /= 255, b /= 255;
+			var max = Math.max(r, g, b), min = Math.min(r, g, b);
+			var h, s, l = (max + min) / 2;
+			if(max === min) {
+				h = s = 0; // achromatic
 			} else {
-				var hue2rgb = function hue2rgb(p, q, t) {
-					if(t < 0) t += 1;
-					if(t > 1) t -= 1;
-					if(t < 1/6) return p + (q - p) * 6 * t;
-					if(t < 1/2) return q;
-					if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-					return p;
-				};
-				var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-				var p = 2 * l - q;
-				r = hue2rgb(p, q, h + 1/3);
-				g = hue2rgb(p, q, h);
-				b = hue2rgb(p, q, h - 1/3);
+				var d = max - min;
+				s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+				switch(max) {
+				case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+				case g: h = (b - r) / d + 2; break;
+				case b: h = (r - g) / d + 4; break;
+				}
+				h /= 6;
 			}
-			return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+			return [h, s, l];
 		}
-
-		var hash = input.split('-').join('');
-		var result = 0;
+		var hash = input.toLowerCase().replace(/[^0-9a-f]+/g, '');
+		var result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+		var rgb = [0, 0, 0];
 		var sat = 80;
 		var lum = 68;
+		var modulo = 16;
 		for(var i in hash) {
-			result += parseInt(hash.charAt(i), 16)/16;
+			result[i%modulo] = result[i%modulo] + parseInt(hash.charAt(i), 16).toString();
 		}
-		result = result * 360;
-		var rgb = hslToRgb(result, sat, lum);
+		for(var count=0;count<modulo;count++) {
+			rgb[count%3] += parseInt(result[count]);
+		}
+		var hsl = rgbToHsl(rgb[0], rgb[1], rgb[2]);
 		var bright = Math.sqrt( 0.299 * Math.pow(rgb[0], 2) + 0.587 * Math.pow(rgb[1], 2) + 0.114 * Math.pow(rgb[2], 2) );
 		if (bright >= 200) {
 			sat = 60;
 		}
-		return 'hsl('+result+', '+sat+'%, '+lum+'%)';
+		var hue = parseInt(hsl[0]*360);
+		return 'hsl('+hue+', '+sat+'%, '+lum+'%)';
 	};
 });
