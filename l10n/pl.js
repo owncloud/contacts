ebcc21836962a10fc397b7f547752a51d0b9c667
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-01-19 00:48:17 +0100

    [tx-robot] updated from transifex

--- l10n/pl.js
+++ l10n/pl.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Contacts" : "Kontakty",
     "Download" : "Pobierz",
+    "ShowURL" : "Pokaż adres URL",
     "Share with users or groups" : "Współdziel z grupami bądź użytkownikami",
     "Delete" : "Usuń",
     "can edit" : "może edytować",
