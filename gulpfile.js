7488d4acf799ea0f65986d0a7bce0e50d851f564
Jannik Stehle <jannik.stehle@gmail.com>
2021-04-14 11:19:30 +0200

    Update to gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -5,40 +5,6 @@ var gulp = require('gulp'),
 	KarmaServer = require('karma').Server,
 	sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', ['eslint'], function() {
-	return gulp.src([
-			'js/main.js',
-			'js/components/**/*.js',
-			'js/models/**/*.js',
-			'js/services/**/*.js',
-			'js/filters/**/*.js'
-			])
-		// concat (+sourcemaps)
-		.pipe(sourcemaps.init())
-			.pipe(ngAnnotate({ single_quotes: true }))
-			.pipe(concat('script.js'))
-		.pipe(sourcemaps.write())
-
-		.pipe(gulp.dest('js/public'));
-});
-
-gulp.task('eslint', function() {
-	return gulp.src([
-			'js/main.js',
-			'js/components/**/*.js',
-			'js/models/**/*.js',
-			'js/services/**/*.js',
-			'js/filters/**/*.js'
-			])
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
-});
-
-gulp.task('watch', ['default'], function() {
-	gulp.watch(['js/**/*.js', '!js/public/**/*.js'], ['default']);
-});
-
 gulp.task('karma', function(done){
 	KarmaServer.start({
 		configFile: __dirname + '/karma.conf.js',
@@ -47,3 +13,38 @@ gulp.task('karma', function(done){
 		done();
 	});
 });
+
+gulp.task('eslint', function() {
+	return gulp.src([
+		'js/main.js',
+		'js/components/**/*.js',
+		'js/models/**/*.js',
+		'js/services/**/*.js',
+		'js/filters/**/*.js'
+	])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+});
+
+
+gulp.task('default', gulp.series('eslint', function() {
+	return gulp.src([
+		'js/main.js',
+		'js/components/**/*.js',
+		'js/models/**/*.js',
+		'js/services/**/*.js',
+		'js/filters/**/*.js'
+	])
+		// concat (+sourcemaps)
+		.pipe(sourcemaps.init())
+		.pipe(ngAnnotate({ single_quotes: true }))
+		.pipe(concat('script.js'))
+		.pipe(sourcemaps.write())
+
+		.pipe(gulp.dest('js/public'));
+}));
+
+gulp.task('watch', gulp.series('default', function() {
+	gulp.watch(['js/**/*.js', '!js/public/**/*.js'], gulp.series('default'));
+}));
